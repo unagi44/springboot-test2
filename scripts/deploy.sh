8d54f1638842dd0c132774df567b436703a94710
Lee Gye Yeon <unagi44@naver.com>
2022-06-06 23:56:35 +0900

    Travis CI, s3 deploy, aws codedeploy, springboot 실행 테스트2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -24,7 +24,7 @@ JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 echo "> $JAR_NAME 에 실행권한 추가"
-chmod +x $JAR_NAME
+chmod 700 $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 nohup java -jar \
