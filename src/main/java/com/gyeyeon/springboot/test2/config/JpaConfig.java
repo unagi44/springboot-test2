2f4a1b92e796a7153967560eac27d4b7d63603bc
Lee Gye Yeon <unagi44@naver.com>
2022-06-06 15:12:24 +0900

    springboot2 test 개발 완료

---
+++ src/main/java/com/gyeyeon/springboot/test2/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.gyeyeon.springboot.test2.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
--- src/main/java/com/gyeyeon/springboot/test2/web/IndexController.java
+++ src/main/java/com/gyeyeon/springboot/test2/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.gyeyeon.springboot.test2.web;
 
+import com.gyeyeon.springboot.test2.config.auth.LoginUser;
+import com.gyeyeon.springboot.test2.config.auth.dto.SessionUser;
 import com.gyeyeon.springboot.test2.service.posts.PostsService;
 import com.gyeyeon.springboot.test2.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
@@ -15,9 +19,11 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model)
+    public String index(Model model, @LoginUser SessionUser user)
     {
         model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null)
+            model.addAttribute("userName", user.getName());
         return "index";
     }
 
