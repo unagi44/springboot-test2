5dabfefe67a7093596d4522ca518379245ae445a
Lee Gye Yeon <unagi44@naver.com>
2022-06-08 00:48:09 +0900

    무중단 배포 real2 확인

--- scripts/start.sh
+++ scripts/start.sh
@@ -11,7 +11,7 @@ echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY"
 cp $REPOSITORY/zip/*.jar $REPOSITORY
 
-echo"> 새 애플리케이션 배포"
+echo "> 새 애플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
